6fd9d1aa249b1142a7022e11e0a30a5c32e5a8d1
sexkzn <dosugkzn116@yandex.ru>
2019-10-17 15:39:47 +0300

    custom 404

--- src/main/java/com/example/demo/AppConfiguration.java
+++ src/main/java/com/example/demo/AppConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Configuration
@@ -13,6 +15,21 @@ public class AppConfiguration {
         @GetMapping("favicon.ico")
         @ResponseBody
         void returnNoFavicon() {
+        }
+    }
+
+    @Controller
+    public static class MyErrorController implements ErrorController {
+
+        @RequestMapping("/error")
+        public String handleError() {
+            //do something like logging
+            return "error";
+        }
+
+        @Override
+        public String getErrorPath() {
+            return "/error";
         }
     }
 
