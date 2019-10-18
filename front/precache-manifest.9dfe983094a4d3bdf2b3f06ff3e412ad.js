3bca1cb8cf7e2483fbde416b1d8a426d0f652ffc
sexkzn <dosugkzn116@yandex.ru>
2019-10-18 16:15:17 +0300

    front

---
+++ front/precache-manifest.9dfe983094a4d3bdf2b3f06ff3e412ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19fcfb5c6d343b3dda4deb4d1d75d5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e368ff332994d1ac9e29",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3d02bf962cb44a2e6501",
+    "url": "/static/js/2.e76263af.chunk.js"
+  },
+  {
+    "revision": "e368ff332994d1ac9e29",
+    "url": "/static/js/main.6f4ed154.chunk.js"
+  },
+  {
+    "revision": "a2f4467f36581cafe647",
+    "url": "/static/js/runtime-main.f5bc0b73.js"
+  }
+]);
