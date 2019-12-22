f7ddd45b0983d3e5ccd82e9445105ca91ad6d2cc
sexkzn <dosugkzn116@yandex.ru>
2019-12-22 12:57:17 +0300

    change 302 to 200 on POST /login

--- backend/src/main/java/com/example/demo/AppConfiguration.java
+++ backend/src/main/java/com/example/demo/AppConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -10,10 +12,22 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class AppConfiguration {
@@ -54,12 +68,40 @@ public class AppConfiguration {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
+            http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin().successHandler(new CustomAuthenticationSuccessHandler()).and().httpBasic();
         }
 
         @Bean
         public PasswordEncoder passwordEncoder() {
             return new BCryptPasswordEncoder();
+        }
+    }
+
+    public static class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+        protected final Log logger = LogFactory.getLog(this.getClass());
+        private RequestCache requestCache = new HttpSessionRequestCache();
+
+        public CustomAuthenticationSuccessHandler() {
+        }
+
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+            SavedRequest savedRequest = this.requestCache.getRequest(request, response);
+            if (savedRequest == null) {
+                clearAuthenticationAttributes(request);
+            } else {
+                String targetUrlParameter = this.getTargetUrlParameter();
+                if (!this.isAlwaysUseDefaultTargetUrl() && (targetUrlParameter == null || !StringUtils.hasText(request.getParameter(targetUrlParameter)))) {
+                    clearAuthenticationAttributes(request);
+                    String targetUrl = savedRequest.getRedirectUrl();
+                } else {
+                    this.requestCache.removeRequest(request, response);
+                    super.onAuthenticationSuccess(request, response, authentication);
+                }
+            }
+        }
+
+        public void setRequestCache(RequestCache requestCache) {
+            this.requestCache = requestCache;
         }
     }
 
