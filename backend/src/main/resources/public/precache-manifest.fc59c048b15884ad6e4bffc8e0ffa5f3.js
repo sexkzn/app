cfdea4b070ec3cdd26e39c81d898cd876e9be719
sexkzn <dosugkzn116@yandex.ru>
2020-01-30 19:42:36 +0300

    update static

---
+++ backend/src/main/resources/public/precache-manifest.fc59c048b15884ad6e4bffc8e0ffa5f3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4162295a6e7b7c08e5b02188e559b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca82e7cd44a78b877210",
+    "url": "/static/css/2.ddba950e.chunk.css"
+  },
+  {
+    "revision": "80b7297871e724e2be72",
+    "url": "/static/css/main.c21d1ee8.chunk.css"
+  },
+  {
+    "revision": "ca82e7cd44a78b877210",
+    "url": "/static/js/2.8775569c.chunk.js"
+  },
+  {
+    "revision": "80b7297871e724e2be72",
+    "url": "/static/js/main.bdf7f454.chunk.js"
+  },
+  {
+    "revision": "3bdace839c41a21ba969",
+    "url": "/static/js/runtime-main.209fff18.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- backend/src/main/resources/public/service-worker.js
+++ backend/src/main/resources/public/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts('/precache-manifest.d2c65d85e96bd04c7caee26db423b2c1.js');
+importScripts(
+  "/precache-manifest.fc59c048b15884ad6e4bffc8e0ffa5f3.js"
+);
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -33,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL('/index.html'),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
